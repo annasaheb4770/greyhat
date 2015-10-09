f14c0158dac68f79feac0840c6b867cce80811de
Annasaheb Rahane <azr@qad.com>
2015-10-09 23:50:23 +0000

    Changes made by Annasaheb Rahane (azr@qad.com).

--- GreyHat Hybrid/app.js
+++ GreyHat Hybrid/app.js
@@ -1,7 +1,7 @@
 (function() {
     // store a reference to the application object that will be created
     // later on so that we can use it if need be
-    // comment added by Smita
+    // comment added by Anil
     var app = {
         data: {}
     };
